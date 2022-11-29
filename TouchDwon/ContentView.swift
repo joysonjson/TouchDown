fb5d8b5ef18907e1fd1cfe824db4633f75ee01f8
joyson.ps <joyson.ps@helpshift.com>
2022-11-29 13:33:50 +0530

    Initial Commit

---
+++ TouchDwon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TouchDwon
+//
+//  Created by Joyson P S on 29/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TouchDwon/TouchDwonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDwonApp.swift
+//  TouchDwon
+//
+//  Created by Joyson P S on 29/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchDwonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
